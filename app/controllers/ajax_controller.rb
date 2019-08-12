dee0739291558a0747ef14ec05cc1fdd18e9b800
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2019-08-12 11:51:05 +0500

    change json

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -9,16 +9,16 @@ class AjaxController < ApplicationController
   end
 
   def post_leftovers
-    puts "params: ", params
+    # puts "params: ", params
 
     data = leftover_params
-    puts "data[store_id] #{data['store_id']}"
-    store_id = data['store_id']
+    # puts "data[store_id] #{data['store_id']}"
+    store_id = data['storageId']
     # puts "data[counts] #{data['counts']}"
-    data['counts'].each do |c|
-      puts "co #{c[1]} - #{c[1]['product_id']}"
-      fnd = ProductLeftover.find_or_create_by(product_id: c[1]['product_id'], store_id: store_id, date: Date.today)
-      fnd.count = c[1]['count']
+    data['ProductForUpload'].each do |c|
+      puts "co #{c} - #{c['product_id']}"
+      fnd = ProductLeftover.find_or_create_by(product_id: c['id'], store_id: store_id, date: Date.today)
+      fnd.count = c['count']
       fnd.save 
     end
 
