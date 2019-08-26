1979370616bb4cab578819da44344af635fc0af0
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2019-08-26 22:58:28 +0500

    welcome path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || welcome_path
+    stored_location_for(resource) || products_path
   end
 
   # def logged_in_user
